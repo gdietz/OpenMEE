fd9e73fef0aa14fc9f70aea561594992a16953cb
George Dietz <dietzgeorge@gmail.com>
2013-12-13 10:26:30 -0500

    fancier about window

--- about.py
+++ about.py
@@ -9,3 +9,8 @@ class About(QDialog, ui_about.Ui_Dialog):
     def __init__(self, parent=None):
         super(About, self).__init__(parent)
         self.setupUi(self)
+        
+        self.contents.linkActivated.connect(self.open_link)
+        
+    def open_link(self, link_as_qstr):
+        QDesktopServices.openUrl(QUrl(link_as_qstr))
