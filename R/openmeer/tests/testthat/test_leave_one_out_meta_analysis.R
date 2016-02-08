743c8960f9d756ec8252daa7eeadcda335e17094
George Dietz <dietzgeorge@gmail.com>
2016-02-07 22:53:35 -0500

    more R unit tests issue #193

--- R/openmeer/tests/testthat/test_leave_one_out_meta_analysis.R
+++ R/openmeer/tests/testthat/test_leave_one_out_meta_analysis.R
@@ -3,9 +3,76 @@ library(openmeer)
 context("Leave-one-out Meta-Analysis")
 
 # prepare data
+tmp_obj <- new(
+  'ContinuousData',
+  N1=c(60, 65, 40, 200, 45, 85),
+  mean1=c(92.0, 92.0, 88.0, 82.0, 88.0, 92.0),
+  sd1=c(20.0, 22.0, 26.0, 17.0, 22.0, 22.0),
+  N2=c(60, 65, 40, 200, 50, 85),
+  mean2=c(94.0, 98.0, 98.0, 94.0, 98.0, 96.0),
+  sd2=c(22.0, 21.0, 28.0, 19.0, 21.0, 21.0),
+  y=c(
+    -0.0945241585203,
+    -0.277355866266,
+    -0.366544429516,
+    -0.664385099891,
+    -0.461806281288,
+    -0.185164437399
+  ),
+  SE=c(
+  	0.182676111563,
+  	0.176252946255,
+  	0.225476645393,
+  	0.102721757438,
+    0.208193827499,
+    0.153721347104
+  ),
+  study.names=c(
+  	"Carroll, 1997",
+  	"Grant, 1981",
+  	"Peck, 1987",
+  	"Donat, 2003",
+  	"Stewart, 1990",
+  	"Young, 1995"
+  ),
+  years=c(
+  	as.integer(),
+  	as.integer(),
+  	as.integer(),
+  	as.integer(),
+  	as.integer(),
+    as.integer()
+  ),
+  covariates=list()
+)
 
+params <- data.frame(
+	conf.level = 95,
+	digits = 3L,
+	fp_col2_str = "[default]",
+    fp_col3_str = "Ev/Trt",
+    fp_col4_str = "Ev/Ctrl",
+    fp_xticks = "[default]",
+    fp_show_col4 = FALSE,
+    fp_show_col3 = FALSE,
+    fp_xlabel = "[default]",
+    fp_show_col1 = TRUE,
+    fp_show_col2 = TRUE,
+    fp_outpath = "./r_tmp/forest.png",
+    rm.method = "DL",
+    fp_plot_ub = "[default]",
+    fp_col1_str = "Studies",
+    measure = "SMD",
+    fp_plot_lb = "[default]",
+    fp_show_summary_line = TRUE
+)
+
+fname <- 'continuous.random'
 
 test_that("Leave-one-out Meta-Analysis Runs", {
-  # stub until a real unit test is written
-  expect_true(FALSE)
+  set.global.conf.level(95)
+  results <- loo.ma.continuous(fname, tmp_obj, params)
+
+  image.path <- results$images[['Leave-one-out Forest Plot']]
+  expect_true(file.exists(image.path))
 })
--- R/openmeer/tests/testthat/test_meta_regression.R
+++ R/openmeer/tests/testthat/test_meta_regression.R
@@ -3,9 +3,50 @@ library(openmeer)
 context("Meta-Regression")
 
 # prepare data
-
+tmp_obj <- data.frame(
+  yi = c(
+    -0.09452415852033,
+    -0.277355866265512,
+    -0.366544429515919,
+    -0.664385099891136,
+    -0.461806281287715,
+    -0.185164437399104
+  ),
+  vi = c(
+  	0.0333705617355999,
+  	0.0310651010636611,
+  	0.0508397176175572,
+    0.0105517594511967,
+    0.0433446698087316,
+    0.0236302525555215
+  ),
+  STATE = factor(c('MA','MA','MA','RI','RI','RI')),
+  slab = c(
+  	"Carroll, 1997",
+    "Grant, 1981",
+    "Peck, 1987",
+    "Donat, 2003",
+    "Stewart, 1990",
+    "Young, 1995"
+  )
+)
 
 test_that("Meta-Regression Runs", {
-  # stub until a real unit test is written
-  expect_true(FALSE)
+  results <- g.meta.regression(
+    data=tmp_obj,
+    mods=list(
+      interactions = list(),
+      numeric = character(0),
+      categorical = "STATE"
+    ),
+    method="REML",
+    level=95.0,
+    digits=3,
+    measure="SMD",
+    btt=NULL,
+    make.coeff.forest.plot=FALSE,
+    exclude.intercept=FALSE
+  )
+
+  expect_true("Summary" %in% names(results_obj))
 })
